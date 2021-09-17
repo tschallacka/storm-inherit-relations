f5f0cb411c6ea9f24a18b8c898e6baa4483c3bd2
= <mdibbets@outlook.com>
2021-09-17 07:57:09 +0200

    Added the code for the behaviour

--- src/behaviour/InheritRelations.php
+++ src/behaviour/InheritRelations.php
@@ -2,7 +2,33 @@
 
 namespace Tschallacka\StormInheritRelations\Behavior;
 
-class InheritRelations
-{
+use Winter\Storm\Extension\ExtensionBase;
 
+class InheritRelations extends ExtensionBase
+{
+    private static $cache = [];
+
+    public function __construct($parent)
+    {
+        $key = get_class($parent);
+
+        if(!isset(self::$cache[$key])) {
+            $ancestors = class_parents($parent);
+            $base_relations = $parent->getRelationDefinitions();
+
+            foreach ($ancestors as $ancestor) {
+                $instance = $ancestor;
+                if(method_exists($instance, 'getRelationDefinitions')) {
+                    $relations = $instance->getRelationDefinitions();
+                    $base_relations = array_merge_recursive($relations, $base_relations);
+                }
+            }
+            self::$cache[$key] = $base_relations;
+        }
+
+        $relations = self::$cache[$key];
+        foreach ($relations as $type => $defintions) {
+            $parent->{$type} = $defintions;
+        }
+    }
 }
